502e134aaf03c95e1d7694c5f2c6dac354fb7551
makhlu <makhlu@DESKTOP-H292C22>
2017-05-02 18:31:51 +0300

    ServiceBusConnection library for UWP & WinRT

---
+++ SmartHive.LevelMapApp/SmartHive.LevelMapApp.CloudController/Events/OnReadingTaskEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHive.LevelMapApp.CloudController.Events
+{
+    public sealed class OnReadingTaskEventArgs
+    {
+        public string subscriptionAddress { get; set; }
+    }
+}
